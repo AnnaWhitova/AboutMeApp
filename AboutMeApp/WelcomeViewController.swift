233a05544606480ebcfff44afb8711c0a56d59ae
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-05-25 00:24:35 +0300

    set up app logic

--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -7,23 +7,17 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
+final class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var userLabel: UILabel!
+    
+    var user: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userLabel.text = user
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
